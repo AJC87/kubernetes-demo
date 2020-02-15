d4934a817c2ea9f993e5625e3895cece3a7e3c9a
Ashley Craig <ashleycraig19872@hotmail.co.uk>
2020-02-15 12:52:43 +0000

    Create project

---
+++ backend/src/main/java/com/ashleycraig/talkbooker/TalkBookerAPI.java
@@ -0,0 +1,12 @@
+package com.ashleycraig.talkbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkBookerAPI {
+
+	public static void main(String[] args) {
+	    SpringApplication.run(TalkBookerAPI.class, args);
+	}
+}
---
+++ backend/src/test/java/com/ashleycraig/talkbooker/TalkBookerAPITests.java
@@ -0,0 +1,13 @@
+package com.ashleycraig.talkbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkBookerAPITests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
